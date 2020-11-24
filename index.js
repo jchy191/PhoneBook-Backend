03b8ff82b0b9257a86d205d5525a1dfa87d26ed0
Joshua Chin <joshuachin191@gmail.com>
2020-11-24 14:21:53 +0800

    Added ability to retrieve contacts

--- index.js
+++ index.js
@@ -26,9 +26,26 @@ const persons = [
 
 app.use(express.json());
 
+app.get('/info', (req, res) => {
+    const date = new Date()
+    res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`)
+})
+
 app.get('/api/persons', (req, res) => {
     res.status(200).json(persons);
 })
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const person = persons.find(person => person.id === id);
+    if (person) {
+        return res.status(200).json(person);
+    } else {
+        return res.status(404).end();
+    }
+})
+
+
 
 
 const PORT = 3001;
